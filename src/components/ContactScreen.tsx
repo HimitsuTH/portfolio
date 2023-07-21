47c4cee95284061216fb9d894b48a65f7a3552bd
chinnawich <hotlnw159@gmail.com>
2023-07-21 17:33:44 +0700

    Update Contact link

--- src/components/ContactScreen.tsx
+++ src/components/ContactScreen.tsx
@@ -1,6 +1,6 @@
 // import React from "react";
 import GithubIcon from "../assets/code.png";
-import EmailIcon from "../assets/email.png";
+
 import linkedinIcon from "../assets/linkedin.png";
 
 const ContactScreen = () => {
@@ -8,11 +8,12 @@ const ContactScreen = () => {
     <div className=" place-items-center grid overflow-hidden bg-white">
       <div className=" m-5">
         <div className="flex flex-row gap-5">
-          <img src={EmailIcon} alt="email" className=" w-10 h-10" />
-          <img src={GithubIcon} alt="github"  className=" w-10 h-10"/>
-          <img src={linkedinIcon} alt="linkedin"  className=" w-10 h-10"/>
-
-          <p></p>
+          <a href="https://github.com/HimitsuTH">
+            <img src={GithubIcon} alt="github" className=" w-10 h-10" />
+          </a>
+          <a href="https://www.linkedin.com/in/chinnawich-ampai-064353284/">
+            <img src={linkedinIcon} alt="linkedin" className=" w-10 h-10" />
+          </a>
         </div>
       </div>
     </div>
