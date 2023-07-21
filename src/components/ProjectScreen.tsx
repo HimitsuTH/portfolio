0b89d2c2b49444d3a14dd5bad0eff468c1495163
chinnawich <hotlnw159@gmail.com>
2023-07-21 17:39:56 +0700

    update Project Item display

--- src/components/ProjectScreen.tsx
+++ src/components/ProjectScreen.tsx
@@ -1,4 +1,7 @@
 // import React, { useEffect } from "react";
+import internetIcon from "../assets/internet.png";
+import apiIcon from "../assets/api.png";
+import atomIcon from "../assets/atom.png";
 
 import {
   Card,
@@ -13,14 +16,26 @@ const ProjectScreen = () => {
       <div>
         <h1 className="sm:text-4xl text-2xl mb-5 select-none ">Project</h1>
         <div className=" flex flex-row gap-5 flex-wrap items-center justify-center">
-          <Card>
+          <Card className=" p-2 flex justify-center flex-col items-center select-none">
             <CardHeader>Website</CardHeader>
+            <CardContent>
+              <img src={internetIcon} alt="web" />
+            </CardContent>
+            <CardDescription>Web introduce myself</CardDescription>
           </Card>
-          <Card>
+          <Card className=" p-2 flex justify-center flex-col items-center select-none">
             <CardHeader>RestFulApi</CardHeader>
+            <CardContent>
+              <img src={apiIcon} alt="api" />
+            </CardContent>
+            <CardDescription>Web introduce myself</CardDescription>
           </Card>
-          <Card>
+          <Card className=" p-2 flex justify-center flex-col items-center select-none">
             <CardHeader>React Native</CardHeader>
+            <CardContent>
+              <img src={atomIcon} alt="react" />
+            </CardContent>
+            <CardDescription>Web introduce myself</CardDescription>
           </Card>
         </div>
       </div>
