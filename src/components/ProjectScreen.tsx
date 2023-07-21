efac84f40b809252de65176a2164e4664fcb8fb0
chinnawich <hotlnw159@gmail.com>
2023-07-21 17:26:54 +0700

    update project screen

--- src/components/ProjectScreen.tsx
+++ src/components/ProjectScreen.tsx
@@ -1,5 +1,4 @@
 // import React, { useEffect } from "react";
-import data from "../assets/projectItem.json";
 
 import {
   Card,
@@ -14,19 +13,15 @@ const ProjectScreen = () => {
       <div>
         <h1 className="sm:text-4xl text-2xl mb-5 select-none ">Project</h1>
         <div className=" flex flex-row gap-5 flex-wrap items-center justify-center">
-          {data.map((data) => (
-            <Card
-              key={data?.id}
-              className=" py-4  transition-all w-full sm:w-auto mx-10 bg-transparent shadow-none"
-            >
-              <CardHeader className="grid place-items-center">
-                <img src={data.img} className=" w-36 h-40  overflow-x-hidden" />
-              </CardHeader>
-              <CardContent className=" text-center">
-                <CardDescription>{data?.title}</CardDescription>
-              </CardContent>
-            </Card>
-          ))}
+          <Card>
+            <CardHeader>Website</CardHeader>
+          </Card>
+          <Card>
+            <CardHeader>RestFulApi</CardHeader>
+          </Card>
+          <Card>
+            <CardHeader>React Native</CardHeader>
+          </Card>
         </div>
       </div>
     </div>
