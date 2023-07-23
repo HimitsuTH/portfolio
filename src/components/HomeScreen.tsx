dfe986873b389a5e75231f174ecc34778a45bdc8
chinnawich <hotlnw159@gmail.com>
2023-07-23 12:59:38 +0700

    update font size

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -16,10 +16,10 @@ const HomeScreen = () => {
   return (
     <div className="top-section h-screen place-items-center grid">
       <div className="text-center">
-        <h1 className="sm:text-5xl text-2xl mb-5 select-none ">
+        <h1 className="sm:text-5xl text-3xl mb-5 select-none ">
           Hi, I am Chinnawich Ampai
         </h1>
-        <h2 className="sm:text-2xl text-xl">Thai-nichi institute of technology</h2>
+        <h2 className="sm:text-2xl text-lg">Thai-nichi institute of technology</h2>
       </div>
       <div className=" absolute w-60 h-60 right-20 md:block hidden">
         <img src={iconThink} alt="icon" />
