f773b77b6f203f2a3cb44ca979912435c2b8338d
chinnawich <hotlnw159@gmail.com>
2023-07-23 12:09:32 +0700

    update `About` content

--- src/components/AboutScreen.tsx
+++ src/components/AboutScreen.tsx
@@ -1,15 +1,11 @@
 // import React from "react";
-
+import EducationImg from "../assets/educationImg.png";
 
 const AboutScreen = () => {
   return (
-    <div
-      className="h-screen  p-10 grid place-items-center"
-      id="about"
-    >
-      <div className="flex items-center">
-        <h1 className=" text-2xl mb-5 select-none mx-10">About</h1>
-        
+    <div className="h-screen  p-10 grid place-items-center" id="about">
+      <div className="flex items-center flex-col">
+        <img src={EducationImg} alt="education" />
       </div>
     </div>
   );
